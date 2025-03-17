57ed62ece1a2b509a60772f3ede00ddf7ff2fd17
Deivit Eduardo <deiviteduardo87@gmail.com>
2025-03-16 21:00:30 -0300

    fix(header): enhance layout and improve accessibility

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,12 +7,12 @@ export function Header() {
   return (
     <header className="h-24 flex items-center justify-between">
       <div className="flex items-center gap-2">
-        <Image src={Logo} alt="TuneReveal logo" className="w-8 h-8" />
-        <p className="text-xl font-medium">TuneReveal</p>
+        <Image src={Logo} alt="TuneReveal logo" priority className="w-8 h-8" />
+        <p className="text-xl font-medium leading-none hidden min-[20rem]:flex">TuneReveal</p>
       </div>
 
-      <div className="flex gap-x-1.5">
-        <p className="text-foreground text-sm font-medium">
+      <div className="flex items-center gap-x-2">
+        <p className="text-foreground leading-none text-sm font-medium">
           Built by{' '}
           <Link href="https://duardodev.vercel.app/en" target="_blank" className="underline-offset-2 underline">
             duardodev
