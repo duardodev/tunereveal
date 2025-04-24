d3520f8d105fb1de6a298e35c6853e2c72b9e3ad
Deivit Eduardo <deiviteduardo87@gmail.com>
2025-04-24 12:56:01 -0300

    add initial backend files

---
+++ backend/src/test/java/com/tunereveal/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tunereveal.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
