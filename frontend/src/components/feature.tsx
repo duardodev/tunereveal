0aa21ea944206fa0ffbabf6fe6d75329a1146eb5
Deivit Eduardo <deiviteduardo87@gmail.com>
2025-06-05 13:12:50 -0300

    refactor: update background gradient and text color for better visibility

--- frontend/src/components/feature.tsx
+++ frontend/src/components/feature.tsx
@@ -13,13 +13,13 @@ export function Feature({ heading, paragraph, icon: Icon }: FeatureProps) {
       className={cn(
         'flex flex-col md:flex-row w-full md:max-w-[450px] animate-shine items-center gap-5 rounded-xl md:border',
         'border-border bg-[length:400%_100%]',
-        'md:p-4 transition-colors md:dark:bg-[linear-gradient(110deg,#14151a,45%,#1d1e25,55%,#14151a)]'
+        'md:p-4 transition-colors md:bg-[linear-gradient(110deg,#0e0e12,45%,#17171d,55%,#0e0e12)]'
       )}
     >
       <Icon className="h-12 max-w-12 sm:h-14 sm:max-w w-full text-red" color="#fb2c36" />
       <div className="max-w-2xs lg:max-w-full">
-        <h2 className="text-foreground/95 text-xl md:text-2xl text-center md:text-start font-medium">{heading}</h2>
-        <p className="sm:text-lg text-center md:text-start text-foreground/90">{paragraph}</p>
+        <h2 className="text-foreground/90 text-xl md:text-2xl text-center md:text-start font-medium">{heading}</h2>
+        <p className="sm:text-lg text-center md:text-start text-foreground/85">{paragraph}</p>
       </div>
     </div>
   );
