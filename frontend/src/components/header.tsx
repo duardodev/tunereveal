cfb553faeea9462f904b16e710a983f6b789a212
Deivit Eduardo <deiviteduardo87@gmail.com>
2025-06-16 12:54:02 -0300

    refactor: update Header with GitHub repository link

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,4 +1,5 @@
-import { ExternalLink } from 'lucide-react';
+import { ExternalLink, Github } from 'lucide-react';
+import { Button } from './ui/button';
 import Image from 'next/image';
 import Link from 'next/link';
 import Logo from '../../public/logo.svg';
@@ -11,14 +12,23 @@ export function Header() {
         <p className="text-xl text-foreground/95 font-medium leading-none hidden min-[20rem]:flex">TuneReveal</p>
       </div>
 
-      <div className="flex items-center gap-x-2">
-        <p className="text-foreground leading-none text-sm font-medium">
-          Built by{' '}
-          <Link href="https://duardodev.vercel.app/en" target="_blank" className="underline-offset-4 underline">
-            duardodev
+      <div className="flex items-center gap-x-8">
+        <div className="flex items-center gap-x-2">
+          <p className="text-foreground leading-none text-sm font-medium">
+            Built by{' '}
+            <Link href="https://duardodev.vercel.app/en" target="_blank" className="underline-offset-4 underline">
+              duardodev
+            </Link>
+          </p>
+          <ExternalLink size={16} />
+        </div>
+
+        <Button asChild size="sm" className="h-7 bg-foreground hover:bg-foreground/90 text-zinc-900">
+          <Link href="https://github.com/duardodev/tunereveal" target="_blank">
+            <Github />
+            GitHub
           </Link>
-        </p>
-        <ExternalLink size={16} />
+        </Button>
       </div>
     </header>
   );
