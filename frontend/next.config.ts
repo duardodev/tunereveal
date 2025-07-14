cde84dc6b4c72a18144aed8055703cdc7d92b7da
Deivit Eduardo <deiviteduardo87@gmail.com>
2025-07-14 10:50:28 -0300

    feat: add Buy Me A Coffee button

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.buymeacoffee.com',
+        port: '',
+        pathname: '/buttons/v2/default-*.png',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -6,21 +6,31 @@ import Logo from '../../public/logo.svg';
 
 export function Header() {
   return (
-    <header className="h-24 flex items-center justify-between">
+    <header className="h-28 py-4 flex flex-col sm:flex-row gap-5 items-center justify-between">
       <div className="flex items-center gap-2">
         <Image src={Logo} alt="TuneReveal logo" priority className="border border-border rounded-lg z-10 w-8 h-8" />
         <p className="text-lg text-foreground/95 font-medium leading-none hidden min-[20rem]:flex">TuneReveal</p>
       </div>
 
-      <div className="flex items-center gap-x-7">
-        <Link href="#how-it-works" className="text-gray-300 hover:text-white transition-colors leading-none text-sm">
+      <div className="flex items-center gap-x-5">
+        <Link href="#how-it-works" className=" text-gray-200 hover:text-white transition-colors leading-none text-sm">
           How it Works
         </Link>
 
-        <Button asChild size="sm" className="h-7 bg-foreground hover:bg-foreground/90 text-zinc-900">
+        <Link href="https://www.buymeacoffee.com/tunereveal" target="_blank">
+          <Image
+            src="https://cdn.buymeacoffee.com/buttons/v2/default-red.png"
+            alt="Buy Me A Coffee"
+            width={160}
+            height={40}
+            className="w-32 h-8 hover:scale-105 transition-transform duration-200"
+            priority
+          />
+        </Link>
+
+        <Button asChild size="sm" className="h-8 bg-foreground hover:bg-foreground/90 text-zinc-900">
           <Link href="https://github.com/duardodev/tunereveal" target="_blank">
             <Github />
-            <span className="hidden sm:flex">GitHub</span>
           </Link>
         </Button>
       </div>
