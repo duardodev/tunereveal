e362c840bdbc97507297d5991157521e70fac75f
Deivit Eduardo <deiviteduardo87@gmail.com>
2026-01-31 16:45:11 -0300

    fix(downloader): specify audio format to prioritize m4a in download command

--- backend/python/app/audio/downloader.py
+++ backend/python/app/audio/downloader.py
@@ -39,7 +39,7 @@ def download_audio(video_url, base_path):
             "--user-agent",
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
             "--add-header", "Accept-Language:en-US,en;q=0.9",
-            "-f", "bestaudio/best",
+            "-f", "bestaudio[ext=m4a]/bestaudio/best",
             "--no-playlist",
             "--no-warnings",
             "--newline",
